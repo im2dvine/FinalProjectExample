9757032134f8f8669731ea473163fb1314b32b22
Daesy Vences <daesydesign@gmail.com>
2021-07-31 21:31:16 -0500

    updated by Daesy

---
+++ public/index.js
@@ -0,0 +1,8 @@
+const localContextMapView = new google.maps.localContext.LocalContextMapView({
+    element: document.getElementById("map"),
+    placeTypePreferences: [
+      { type: "restaurant" },
+      { type: "tourist_attraction" },
+    ],
+    maxPlaceCount: 12,
+  });
